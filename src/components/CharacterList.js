4b9565cc13df5377cad14c8a5947c0f5d72b01dc
Matthew John Orth <matthewjohnorth@Matthews-MacBook-Pro-72.local>
2019-10-13 21:30:17 -0700

    mvp

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,62 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+import styled from "styled-components";
+import SearchForm from "./SearchForm";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
+
+  const [characters, setCharacters] = useState([])
+  const [search, setSearch] = useState("");
+  
+  const Home = styled.h1`
+    font-size: 3rem;
+    color: blue;
+  `;
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    const getCharacters = () => {
+      axios
+        .get(`https://rickandmortyapi.com/api/character//`)
+        .then(response => {
+          console.log(response);
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.log('Server Error', error);
+        });
+    }
 
+    getCharacters();
+  }, []);
+  
+  console.log(characters);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setSearch(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submitting...');
+  }
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <div onClick={() => {
+        console.log(props);
+        props.history.push('/');
+      }}>
+        <Home>Home</Home>
+        <SearchForm handleChange={handleChange} handleSubmit={handleSubmit}/>
+        
+      </div>
+      {characters.map(character => (
+        <CharacterCard key={character.id} character={character} />
+    ))}
     </section>
   );
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import CharacterCard from "./CharacterCard";
+import { Route } from "react-router-dom";
 
-export default function SearchForm() {
- 
+export default function SearchForm({placeholder, handleChange, handleSubmit}) {
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={handleSubmit}>
+        <input name='search' type='search' placeholder={placeholder} onChange={handleChange} />
+        <button>Submit</button>
+      </form>
     </section>
-  );
+  
+  )
+  
 }
